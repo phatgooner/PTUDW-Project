36c9088a106bc58e53cd69fe1bb970345a0396a8
Phat Gooner <nguyentienphat1604@gmail.com>
2025-03-05 11:47:47 +0700

    categories: update

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,11 +1,24 @@
 const controller = {};
-const models = require('../models')
-controller.show = async (req, res) => {
-    let products = await models.Product.findAll({
-        attibutes: ['id', 'name', 'imagePath', 'stars', 'price', 'oldPrice']
-    });
+const { where } = require('sequelize');
+const models = require('../models');
 
-    res.render('product-list', { products });
+controller.show = async (req, res) => {
+    let categoryId = isNaN(req.query.category) ? 0 : parseInt(req.query.category);
+    let categories = await models.Category.findAll({
+        include: [{
+            model: models.Product
+        }]
+    });
+    let options = {
+        attibutes: ['id', 'name', 'imagePath', 'stars', 'price', 'oldPrice'],
+        where: {}
+    }
+    if (categoryId > 0) {
+        options.where.categoryId = categoryId;
+    }
+    let products = await models.Product.findAll(options);
+
+    res.render('product-list', { products, categories });
 }
 
 module.exports = controller;
