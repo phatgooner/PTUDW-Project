a00559cc9638e577996ad0d242c7da03d0abc01c
Phat Gooner <nguyentienphat1604@gmail.com>
2025-03-05 15:29:28 +0700

    tags: update

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -19,6 +19,14 @@ controller.show = async (req, res) => {
         }]
     });
 
+    //Lấy các sản phẩm từ request theo tags    
+    let tagId = isNaN(req.query.tag) ? 0 : parseInt(req.query.tag);
+    let tags = await models.Tag.findAll({
+        include: [{
+            model: models.Product //lấy các sản phẩm theo brand
+        }]
+    });
+
     //Lọc các sản phẩm theo brand hoặc category
     let options = {
         attibutes: ['id', 'name', 'imagePath', 'stars', 'price', 'oldPrice'],
@@ -30,10 +38,16 @@ controller.show = async (req, res) => {
     else if (brandId > 0) {
         options.where.brandId = brandId;
     }
+    else if (tagId > 0) {
+        options.include = [{
+            model: models.Tag,
+            where: { id: tagId }
+        }]
+    }
     let products = await models.Product.findAll(options);
 
     //Render trang product-list theo bộ lọc
-    res.render('product-list', { products, categories, brands });
+    res.render('product-list', { products, categories, brands, tags });
 }
 
 module.exports = controller;
