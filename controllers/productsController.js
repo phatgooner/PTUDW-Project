b5282752e188b61a33027ce2b430a1edede781b7
Phat Gooner <nguyentienphat1604@gmail.com>
2025-03-12 20:29:41 +0700

    product details: update

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,30 +2,37 @@ const controller = {};
 const { where } = require('sequelize');
 const models = require('../models');
 
-controller.show = async (req, res) => {
-    //Lấy các sản phẩm từ request theo category
-    let categoryId = isNaN(req.query.category) ? 0 : parseInt(req.query.category);
+controller.getData = async (req, res, next) => {
+    //Lấy các sản phẩm từ request theo category    
     let categories = await models.Category.findAll({
         include: [{
             model: models.Product //lấy các sản phẩm theo category
         }]
     });
+    res.locals.categories = categories;
 
-    //Lấy các sản phẩm từ request theo brand    
-    let brandId = isNaN(req.query.brand) ? 0 : parseInt(req.query.brand);
+    //Lấy các sản phẩm từ request theo brand   
     let brands = await models.Brand.findAll({
         include: [{
             model: models.Product //lấy các sản phẩm theo brand
         }]
     });
+    res.locals.brands = brands;
 
     //Lấy các sản phẩm từ request theo tags    
-    let tagId = isNaN(req.query.tag) ? 0 : parseInt(req.query.tag);
     let tags = await models.Tag.findAll({
         include: [{
             model: models.Product //lấy các sản phẩm theo brand
         }]
     });
+    res.locals.tags = tags;
+    next();
+}
+
+controller.show = async (req, res) => {
+    let categoryId = isNaN(req.query.category) ? 0 : parseInt(req.query.category);
+    let brandId = isNaN(req.query.brand) ? 0 : parseInt(req.query.brand);
+    let tagId = isNaN(req.query.tag) ? 0 : parseInt(req.query.tag);
 
     //Lọc các sản phẩm theo brand hoặc category
     let options = {
@@ -47,7 +54,30 @@ controller.show = async (req, res) => {
     let products = await models.Product.findAll(options);
 
     //Render trang product-list theo bộ lọc
-    res.render('product-list', { products, categories, brands, tags });
+    res.render('product-list', { products });
+}
+
+controller.showDetails = async (req, res) => {
+    let id = isNaN(req.params.id) ? 0 : parseInt(req.params.id);
+
+    let product = await models.Product.findOne({
+        attibutes: ['id', 'name', 'summary', 'stars', 'price', 'oldPrice', 'description', 'specification'],
+        where: { id },
+        include: [{
+            model: models.Image,
+            attibutes: ['name', 'imagePath']
+        }, {
+            model: models.Review,
+            attibutes: ['id', 'review', 'stars', 'createdAt'],
+            include: [{
+                model: models.User,
+                attibutes: ['firstName', 'lastName']
+            }]
+        }]
+    });
+
+    res.render('product-detail', { product });
+
 }
 
 module.exports = controller;
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -4,5 +4,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/productsController');
 
-router.get('/', controller.show);
+router.get('/', controller.getData, controller.show);
+router.get('/:id', controller.getData, controller.showDetails);
 module.exports = router;
