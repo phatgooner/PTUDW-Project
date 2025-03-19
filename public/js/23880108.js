a56a1d00fd835b88daec67c3fd910f8eb6c15229
Phat Gooner <nguyentienphat1604@gmail.com>
2025-03-19 19:22:34 +0700

    update cart

--- public/js/23880108.js
+++ public/js/23880108.js
@@ -1,3 +1,5 @@
+'use strict';
+
 async function addCart(id, quantity) {
     let res = await fetch('/products/cart', {
         method: 'post',
@@ -9,4 +11,24 @@ async function addCart(id, quantity) {
     });
     let json = await res.json();
     document.getElementById('cart-quantity').innerText = `(${json.quantity})`;
+}
+
+async function updateCart(id, quantity) {
+    if (quantity > 0) {
+        let res = await fetch('/products/cart', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({ id, quantity })
+        });
+        if (res.status == 200) {
+            let json = await res.json();
+            document.getElementById('cart-quantity').innerText = `(${json.quantity})`;
+            document.getElementById('subtotal').innerText = `$${json.subtotal}`;
+            document.getElementById('total').innerText = `$${json.total}`;
+            document.getElementById(`total${id}`).innerText = `$${json.item.total}`;
+        }
+    }
 }
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -5,8 +5,15 @@ const router = express.Router();
 const controller = require('../controllers/productsController');
 const cartController = require('../controllers/cartController');
 
+//GET Method
 router.get('/', controller.getData, controller.show);
+router.get('/cart', cartController.show);
 router.get('/:id', controller.getData, controller.showDetails);
 
+//POST Method
 router.post('/cart', cartController.add);
+
+//PUT Method
+router.put('/cart', cartController.update);
+
 module.exports = router;
