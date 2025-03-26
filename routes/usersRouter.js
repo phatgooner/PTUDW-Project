3f496b06799950326aaf9081b42bd60a879c886f
Phat Gooner <nguyentienphat1604@gmail.com>
2025-03-26 17:03:44 +0700

    update checkout form submit

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const controller = require('../controllers/usersController');
 
 router.get('/checkout', controller.checkout);
+router.post('/placeorders', controller.placeorders);
 
 module.exports = router;
