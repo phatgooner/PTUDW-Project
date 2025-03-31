2388ecf08fe3f3d8ac93cc9f48e9a80fd3e24e4a
Phat Gooner <nguyentienphat1604@gmail.com>
2025-03-31 11:44:06 +0700

    update form validation

--- public/js/23880108.js
+++ public/js/23880108.js
@@ -82,3 +82,15 @@ async function clearCart() {
         }
     }
 }
+
+async function placeorders(e) {
+    e.preventDefault();
+
+    const addressId = document.querySelector('input[name=addressId]:checked');
+    if (!addressId || addressId.value == 0) {
+        if (!e.target.checkValidity()) {
+            return e.target.reportValidity();
+        }
+    }
+    e.target.submit();
+}
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,8 +3,28 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/usersController');
+const { body, validationResult } = require('express-validator');
 
 router.get('/checkout', controller.checkout);
-router.post('/placeorders', controller.placeorders);
+router.post('/placeorders',
+    body('firstName').notEmpty().withMessage('First name is required!'),
+    body('lastName').notEmpty().withMessage('Last name is required!'),
+    body('email').notEmpty().withMessage('Email is required!').isEmail().withMessage('Invalid email address!'),
+    body('mobile').notEmpty().withMessage('Mobile number is required!'),
+    body('address').notEmpty().withMessage('Address is required!'),
+    (req, res, next) => {
+        let error = validationResult(req);
+        if (req.body.addressId == "0" && !error.isEmpty()) {
+            let errorArray = error.array();
+            let message = '';
+            errorArray.forEach(err => {
+                message += err.msg + "<br>";
+            })
+            return res.render('error', { message });
+
+        }
+        next();
+    },
+    controller.placeorders);
 
 module.exports = router;
